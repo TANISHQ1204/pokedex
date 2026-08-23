0ccdcf374fa9deee72454a3df4c066a6fded30e7
TANISHQ1204 <tanishrastogi11a@gmail.com>
2026-08-23 12:40:32 +0530

    fix: resolve JSX unclosed ternary tag in Collection.jsx

--- src/pages/Collection.jsx
+++ src/pages/Collection.jsx
@@ -576,7 +576,7 @@ export default function Collection() {
                   </div>
                 </div>
               </div>
-            </div>
+            )}
           </div>
         </div>
       )}
